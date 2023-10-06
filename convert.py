56cbc44b5176465031dd8508f531876cfbc323d6
Robbi <robert13grimley@gmail.com>
2023-10-05 22:50:24 -0500

    Added latex->svg script

---
+++ convert.py
@@ -0,0 +1,21 @@
+from tex2svg import latex2svg
+
+# Convert LaTeX code to SVG
+svg = latex2svg(r'\int_0^1 x^2 dx')
+
+# Save SVG to file
+with open('output.svg', 'w') as f:
+    f.write(svg)
+
+import subprocess
+
+# Convert LaTeX code to DVI
+latex_code = r'\int_0^1 x^2 dx'
+dvi = subprocess.check_output(['latex', '-interaction=nonstopmode', '-halt-on-error', '-output-format=dvi', '-jobname=output', '-'], input=latex_code.encode())
+
+# Convert DVI to SVG
+svg = subprocess.check_output(['dvisvgm', '--no-fonts', '-o', 'output.svg', 'output.dvi'], input=dvi)
+
+# Save SVG to file
+with open('output.svg', 'wb') as f:
+    f.write(svg)
