c6fc0a660c2d9fde6a872d767043f67159d85da5
rgri <robert13grimley@gmail.com>
2023-10-09 03:03:25 -0500

    Made convert.sh use proper directories
    As required by dataset.py from untexify.

--- convert.sh
+++ convert.sh
@@ -1,9 +1,9 @@
 #!/run/current-system/sw/bin/bash
 while read line
 do
-    tex2svg $line ./out/$line.svg
-    inkscape -D -w 256 -h 256 ./out/$line.svg -o ./out/$line.png
-    rm ./out/$line.svg
+    tex2svg $line ./original_images/$line.svg
+    inkscape -D -w 256 -h 256 ./original_images/$line.svg -o ./original_images/$line.png
+    rm ./original_images/$line.svg
 
 
 done < equations.txt
