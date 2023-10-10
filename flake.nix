5212c92bbb723b36cda4b22fd845ae524945be40
rgri <robert13grimley@gmail.com>
2023-10-10 03:11:16 -0500

    Thing

--- flake.nix
+++ flake.nix
@@ -9,6 +9,14 @@
         system = "x86_64-linux";
       in {
         devShells.default = pkgs.mkShell {
+
+          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [
+            pkgs.gcc-unwrapped.lib
+            pkgs.libglvnd
+            pkgs.glib
+            # Only necessary for transform.py
+            pkgs.zlib
+          ];
           #TODO: Package tex2svg (github:yannikschaelte/tex2svg) and declare it here
           #BUG: 1d0f74df-8115-4a4c-998d-ae13caad50ef
           packages = [ pkgs.pdf2svg pkgs.inkscape ];
